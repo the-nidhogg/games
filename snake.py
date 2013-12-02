f27fed910af88714a34d676df8c97615b60ff10d
the-nidhogg <balto_59@hotmail.fr>
2013-12-02 10:50:22 +0100

    Update snake.py

--- snake.py
+++ snake.py
@@ -24,8 +24,8 @@ class Snake(object):
 		self.tete[0] += self.vitesse * self.sens[0]
 		self.tete[1] += self.vitesse * self.sens[1]
 		if self.tete[0] < 0 or self.tete[0] >= 600 or \
-			self.tete[1] < 0 or self.tete[1] >= 400 or self.tete in self.corps:
-				return 0
+		self.tete[1] < 0 or self.tete[1] >= 400 or self.tete in self.corps:
+			return 0
 		return 1
 
 	def add(self):
